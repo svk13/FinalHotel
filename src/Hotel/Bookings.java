2d5a262443ed37cccb51340baec355d5f3fd5bc8
svk13@hi.is <svk13@hi.is>
2016-03-24 20:15:57 +0000

    bookings uppfærlsa

--- src/Hotel/Bookings.java
+++ src/Hotel/Bookings.java
@@ -11,8 +11,8 @@ public class Bookings {
 	private int nrOfRooms;
 	private String clientid;
 	private String client_passw;
+	
 	public Bookings(int id, int resid, String datein1, String dateout1, int nrRoom, String client, String passw){
-		
 		hotelID = id;
 		reservationID = resid;
 		dateins = datein1;
@@ -20,33 +20,7 @@ public class Bookings {
 		nrOfRooms=nrRoom;
 		clientid=client;
 		client_passw=passw;
-		makebooking();
-		
-		
-		
+		sqlWorkBench.makebooking(hotelID, reservationID, dateins, dateouts, nrOfRooms,clientid,client_passw);
 	}
-	
-	public java.sql.Date convertJavaDateToSqlDate(java.util.Date date) {
-	    return new java.sql.Date(date.getTime());
-	}
-	
-	public void makebooking(){
-		
-		try{	
-			
-			String qry = "INSERT INTO Room_Bookings Values("+hotelID+","+reservationID+",'"+dateins+"','"+dateouts+"',"+
-					nrOfRooms+",'"+clientid+"','"+client_passw+"');";
-			System.out.println(qry);
-			
-			Statement stmt = Front.connection.createStatement();
-			stmt.setQueryTimeout(30);
-			stmt.executeUpdate(qry);
-				
-					//sqliteConnection.closeConnection(rs, statement, Front.connection)
-		}catch(Exception e2){
-			System.out.println(e2);
-		}
-	}
-	
 	
 }
